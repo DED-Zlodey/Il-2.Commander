be353b3d259f238949714d2694d734defc45511d
Zlodey <ru-net1@yandex.ru>
2021-02-12 05:49:29 +0800

    Изменены таблицв инпутов и полевых складов. Добавлены поля ассоциативных имен и войсов

--- Il-2.Commander/Data/BattlePonts.cs
+++ Il-2.Commander/Data/BattlePonts.cs
@@ -8,6 +8,10 @@ namespace Il_2.Commander.Data
         public int WHID { get; set; }
         public int Point { get; set; }
         public int Coalition { get; set; }
+        public string AssociateNameRU { get; set; }
+        public string AssociateNameEN { get; set; }
+        public string VoiceRU { get; set; }
+        public string VoiceEN { get; set; }
         public int CompareTo(BattlePonts other) //сортировка по по полю Point
         {
             if (other == null)
--- Il-2.Commander/Data/ServerInputs.cs
+++ Il-2.Commander/Data/ServerInputs.cs
@@ -14,5 +14,9 @@
         public int Enable { get; set; }
         public bool Rotation { get; set; }
         public double YOri { get; set; }
+        public string NameRu { get; set; }
+        public string NameEn { get; set; }
+        public string VoiceRu { get; set; }
+        public string VoiceEn { get; set; }
     }
 }
